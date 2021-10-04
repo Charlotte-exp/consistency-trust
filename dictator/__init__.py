771c53eae92b45b70d3091692437fafa48fa33ff
Charlotte <rossetti@evolbio.mpg.de>
2021-10-04 16:31:01 +0200

    trying the new partner every round method

--- dictator/__init__.py
+++ dictator/__init__.py
@@ -13,9 +13,11 @@ S285-S300.
 
 class Constants(BaseConstants):
     name_in_url = 'dictator'
-    players_per_group = 2
+    players_per_group = None
     num_rounds = 3
     instructions_template = 'dictator/instructions.html'
+
+    dictator_role = 'dictator'
 
     pot_money = cu(100)
     endowment_p2 = pot_money/2
@@ -26,6 +28,26 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
     pass
+
+
+def creating_session(subsession: Subsession):
+    session = subsession.session
+    session.past_groups = []
+
+
+def group_by_arrival_time_method(subsession: Subsession, waiting_players):
+    session = subsession.session
+
+    import itertools
+
+    for possible_group in itertools.combinations(waiting_players, 2):
+        # use a set, so that we can easily compare even if order is different
+        # e.g. {1, 2} == {2, 1}
+        pair_ids = set(p.id_in_subsession for p in possible_group)
+        if pair_ids not in session.past_groups:
+            # mark this group as used, so we don't repeat it in the next round.
+            session.past_groups.append(pair_ids)
+            return possible_group
 
 
 class Group(BaseGroup):
@@ -79,6 +101,13 @@ class Offer(Page):
         return dict(partner=player.get_others_in_group()[0])
 
 
+class DictatorPage(Page):
+
+    @staticmethod
+    def is_displayed(player):
+        return player.role == Constants.dictator_role
+
+
 class Receiver(Page):
 
     def is_displayed(player: Player):
@@ -102,6 +131,11 @@ class Results(Page):
         )
 
 
+class PairingWaitPage(WaitPage):
+    group_by_arrival_time = True
+    body_text = "Waiting to pair you with someone you haven't already played with"
+
+
 class End(Page):
     def is_displayed(player: Player):
         if player.round_number == 3:
@@ -117,6 +151,7 @@ class End(Page):
 
 page_sequence = [Introduction,
                  Offer,
+                 DictatorPage,
                  Receiver,
                  ResultsWaitPage,
                  Results,
