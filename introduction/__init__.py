08127ca4a65746e3dea1d5efcaf9e4e7d110ac56
Charlotte <rossetti@evolbio.mpg.de>
2021-10-25 17:31:46 +0200

    added attention check questions to the introductions (temporary ones, they're not really good)

--- introduction/__init__.py
+++ introduction/__init__.py
@@ -46,6 +46,56 @@ class Player(BasePlayer):
 
     title = models.StringField()
 
+    q1 = models.IntegerField(
+            choices=[
+                [1, '0 other participants'],
+                [2, '1 other participant'],
+                [3, 'multiple other participants']
+            ],
+            verbose_name='With how many participants will you be playing in this task?',
+            widget=widgets.RadioSelect
+        )
+
+    q2 = models.IntegerField(
+        choices=[
+            [1, 'There is no bonus possible in this study.'],
+            [2, 'My bonus payment depends on the conversion rates in each round.'],
+            [3, 'My bonus payment depends on a decision taken by one of the participants.']
+        ],
+        verbose_name='What will your bonus payment depend on?',
+        widget=widgets.RadioSelect
+    )
+
+    q3 = models.IntegerField(
+        choices=[
+            [1, 'Participant 1 (You).'],
+            [2, f'Participant 2.'],
+            [3, f'Both Participant 1 and Participant 2.']
+        ],
+        verbose_name=f'Who takes a decision in each round?',
+        widget=widgets.RadioSelect
+    )
+
+    q4 = models.IntegerField(
+        choices=[
+            [1, '0 tokens.'],
+            [2, f'{Constants.endowment_p1} tokens.'],
+            [3, f'{Constants.pot_money} tokens.']
+        ],
+        verbose_name=f'What will be your total payoff in this round if you choose to take the {Constants.endowment_p1} tokens?',
+        widget=widgets.RadioSelect
+    )
+
+    q5 = models.IntegerField(
+        choices=[
+            [1, 'Participant 1 (You).'],
+            [2, f'Participant 2.'],
+            [3, f'Both Participant 1 and Participant 2.']
+        ],
+        verbose_name=f'Who makes a decision in each round?',
+        widget=widgets.RadioSelect
+    )
+
 
 # def set_title(player: Player):
 #     if player.id_in_subsesion % 2 == 0:
@@ -56,7 +106,14 @@ class Player(BasePlayer):
 
 # PAGES
 class Welcome(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['q1', 'q2']
+
+    def error_message(player, values):
+        if values['q1'] != 3:
+            return 'Answer to question 2 is incorrect. Check the instructions again and give a new answer'
+        if values['q2'] != 2:
+            return 'Answer to question 3 is incorrect. Check the instructions again and give a new answer'
 
 
 class Introduction(Page):
@@ -64,27 +121,42 @@ class Introduction(Page):
 
 
 class InstruDictator(Page):
+    form_model = 'player'
+    form_fields = ['q3', 'q4']
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.participant.vars['title'] == 'dictator'
+        return player.participant.title == 'dictator'
+
+    @staticmethod
+    def error_message(player, values):  # it works but the message is wrong...
+        if values['q3'] != 1:
+            return 'Answer to question 2 is incorrect. Check the instructions again and give a new answer'
+        if values['q4'] != 3:
+            return 'Answer to question 3 is incorrect. Check the instructions again and give a new answer'
 
     def vars_for_template(player: Player):
         return {
-            'my_title': player.participant.vars['title'],
+            'my_title': player.participant.title,
         }
 
 
 class InstruReceiver(Page):
-
+    form_model = 'player'
+    form_fields = ['q5']
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.participant.vars['title'] == 'receiver'
+        return player.participant.title == 'receiver'
+
+    @staticmethod
+    def error_message(player, values):
+        if values['q5'] != 2:
+            return 'Answer to question 2 is incorrect. Check the instructions again and give a new answer'
 
     def vars_for_template(player: Player):
         return {
-            'my_title': player.participant.vars['title'],
+            'my_title': player.participant.title,
         }
 
 
