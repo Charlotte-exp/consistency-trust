6a92897a9e1480e7d8a55da30b7c573cc8cdfbe9
Charlotte <rossetti@evolbio.mpg.de>
2022-11-02 15:38:54 +0100

    changed show up fee and wiatroom prolific link

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.50, doc=""
+    real_world_currency_per_point=1.00, participation_fee=0.80, doc=""
 )
 
 SESSION_FIELDS = []
