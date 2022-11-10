ee07c28e9be6039fac8895ea1748eb1c76568d60
Charlotte <rossetti@evolbio.mpg.de>
2022-11-10 16:09:00 +0000

    selected the four treatments that are much less than the others (true randomness is not great here)

--- deception_task/__init__.py
+++ deception_task/__init__.py
@@ -29,8 +29,7 @@ class C(BaseConstants):
 class Subsession(BaseSubsession):
 
     def get_treatments(self):
-        treatments = ["high_high_high", 'high_high_low', 'high_low_high', 'high_low_low',
-                      "low_high_high", 'low_high_low', 'low_low_high', 'low_low_low', ]
+        treatments = ["high_high_high", 'high_low_low', 'low_high_low', 'low_low_high']
         treatment = random.choice(treatments)
         # print(treatment)
         return treatment
