db7759e5a1496cf32251d9558cafb25752ca2a11
Charlotte <rossetti@evolbio.mpg.de>
2024-11-07 14:57:21 +0100

    mechanism to set benefit and cost per round. the function sample two items from a list and checks they match our restrictions before saving them as player fields. At the moment the function is called on the same page which means the numbers change everytime the page is refreshed. good for now for testing but will have to be moved to another "invisible" page.

--- threshold_dictator/__init__.py
+++ threshold_dictator/__init__.py
@@ -1,5 +1,7 @@
 from otree.api import *
 
+import random
+import itertools
 
 doc = """
 Your app description
@@ -9,7 +11,7 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'threshold_dictator'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 3
+    NUM_ROUNDS = 6
 
     endowment = cu(10)
     conversion_rate = 1
@@ -26,7 +28,10 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    receiver_payoff = models.CurrencyField()
+    receiver_payoff = models.CurrencyField(initial=cu(0))
+
+    cost = models.CurrencyField(initial=cu(0))
+    benefit = models.CurrencyField(initial=cu(0))
 
     decision = models.CurrencyField(
         choices=[
@@ -36,6 +41,19 @@ class Player(BasePlayer):
         verbose_name='Your decision:',
         widget=widgets.RadioSelect
     )
+
+    def get_benefits(player):
+        numbers = list(range(1, 9))
+        while True:
+            # sample two numbers with replacement (for without use random.sample(numbers, 2)
+            number_1, number_2 = random.choices(numbers, k=2)
+            # check if sampled numbers satisfy the condition
+            if number_1 + number_2 >= 10 and number_1 <= number_2:
+                player.cost = number_1
+                player.benefit = number_2
+                print('cost', player.cost, 'benefit', player.benefit)
+                return player.cost, player.benefit
+
 
 
 ######## PAGES ##########
@@ -47,8 +65,11 @@ class Decision(Page):
 
     def vars_for_template(player: Player):
         return dict(
-            proba=C.proba_implementation * 10,
+            call_benefits=player.get_benefits(), # has to be on another page or they can change on refresh...
+            proba=int(C.proba_implementation * 100),  # remove decimal from display
             conversion=C.conversion_rate,
+            cost=player.cost,
+            benefit=player.benefit,
         )
 
 
